ea4ecba067f31b83ee1e6bd2461f3b5685e711f5
JyunCun <JyunCun@smart-ecare.com>
2021-04-24 13:46:09 +0800

    任務0 4 ：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { UserInfo } from './../interfaces/user-info';
+import { UserLoginInfo } from './../interfaces/login-info';
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log('onSubmit');
+    const userLogin: UserLoginInfo = {
+      email: 'demo@miniasp.com',
+      password: '123456'
+    };
+    this.loginService.login(userLogin)
+      .subscribe((res: UserInfo) => {
+        localStorage.setItem('JWTToken', res.user.token);
+        this.router.navigate(['/']);
+      });
+  }
 }
